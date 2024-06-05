8bf84158be13506d6d1a6d3d4c72e28a4de7d774
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2024-06-05 23:01:37 +0200

    Minimal reproduction case for escaping slashes
    Run `npm i` and `npx prettier --write foo.tsx`. The bug is visible in
    the git diff.

---
+++ foo.tsx
@@ -0,0 +1,5 @@
+export default () => (
+  <span
+    className={classNames('md:p-2 m-1 m-3 p-4', "before:content-['\\ea53']")}
+  />
+);
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  safelist: ["before:content-['\\ea52']", "before:content-['\\ea53']"],
+  plugins: [],
+};
